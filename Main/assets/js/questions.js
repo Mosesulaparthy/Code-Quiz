572aa78cdfa0b6b008caf7cbca6a55887daf4a7c
Mosesulaparthy <114625182+Mosesulaparthy@users.noreply.github.com>
2023-11-11 18:39:04 -0500

    first commit

---
+++ Main/assets/js/questions.js
@@ -0,0 +1,35 @@
+// list of all questions, choices, and answers
+var questions = [
+  {
+    title: 'Which of the following keywords is used to define a variable in Javascript?',
+    choices: ['a: var', 'b: let', 'c: Both a and b', 'd: none of the above'],
+    answer: 'c: Both a and b',
+  },
+  {
+    title: 'JavaScript is the programming language of the _____.',
+    choices: ['a: Desktop', 'b: Mobile', 'c: Web', 'd: Server'],
+    answer: 'c: Web',
+  },
+  {
+    title: 'Arrays in JavaScript can be used to store ____.',
+    choices: [
+      'a: numbers and strings',
+      'b: other arrays',
+      'c: booleans',
+      'd: all of the above',
+    ],
+    answer: 'd: all of the above',
+  },
+  {
+    title:
+      'String values must be enclosed within ____ when being assigned to variables.',
+    choices: ['a: commas', 'b: curly brackets', 'c: quotes', 'd: parentheses'],
+    answer: 'c: quotes',
+  },
+  {
+    title:
+      'A very useful tool used during development and debugging for printing content to the debugger is:',
+    choices: ['a: JavaScript', 'b: terminal / bash', 'c: for loops', 'd: console.log'],
+    answer: 'd: console.log',
+  },
+];
